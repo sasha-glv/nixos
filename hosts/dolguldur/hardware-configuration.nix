34ebc8862abdb33d4a0e50c1172e6cba7c96b955
Sasha Guljajev <sasha@zxcvmk.com>
2022-12-09 17:05:41 +0100

    Sync

--- hosts/dolguldur/hardware-configuration.nix
+++ hosts/dolguldur/hardware-configuration.nix
@@ -18,7 +18,13 @@
       fsType = "vfat";
     };
 
-  swapDevices = [ ];
+  # Add swap partition
+  swapDevices = [
+    { 
+      device = "/dev/disk/by-uuid/f0dc6723-68d3-497f-aff1-961663ecdc51";
+    }
+  ];
+
 
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
