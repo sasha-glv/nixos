701fa6903ba55131e9a4b4d59fcf4d58b8847934
Sasha Guljajev <sasha@zxcvmk.com>
2023-01-07 19:12:04 +0100

    Move sway config

--- users/sashka/default.nix
+++ users/sashka/default.nix
@@ -53,15 +53,6 @@
 
   wayland.windowManager.sway = {
     enable = true;
-    config = rec {
-      modifier = "Mod4";
-      # Use kitty as default terminal
-      terminal = "kitty";
-      startup = [
-        # Launch Firefox on start
-        {command = "kitty";}
-      ];
-    };
   };
 
   programs = {
